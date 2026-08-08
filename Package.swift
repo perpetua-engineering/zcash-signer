9320e812e94ebee50a979ce2fe807fa55b17be33
Leo <engineering@perpetua.watch>
2026-08-07 22:57:31 -0600

    CR-1491 Bound SwiftPM dependencies with explicit bounds

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.3.0"),
         .package(path: "../zcash-swift-wallet-sdk"),
     ],
     targets: [
